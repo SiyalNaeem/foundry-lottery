b48101deb3fd726eb289f40b77d94add50a278c8
Naeem <siyalnaeem1997@gmail.com>
2025-05-19 17:09:39 +0100

    Started work on project

---
+++ src/Raffle.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.9.19;
+
+
+/**
+* @title A sample Raffle contract
+* @author Naeem Ali
+* @notice This contract is for creating a simple raffle
+* @dev Implements Chainlink VRFv2.5
+ */
+contract Raffle {
+
+    funcion enterRaffle() public {
+
+    }
+
+}
